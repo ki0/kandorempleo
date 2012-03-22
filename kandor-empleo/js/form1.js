e3fa479b16598b805c749d4ea6a40490232d2804
mlastra <mlastra@deee-sign.com>
2012-03-22 20:51:29 +0100

    Last changes in css, loop índex and js

---
+++ kandor-empleo/js/form1.js
@@ -0,0 +1,335 @@
+(function ($){
+    
+    var myArtBox = Backbone.Model.extend({
+        toString: function() { return this.get('title'); }
+    });
+
+    var myTecBox = Backbone.Model.extend({
+        toString: function() { return this.get('title'); }
+    });
+
+    var mySofBox = Backbone.Model.extend({
+        toString: function() { return this.get('title'); }
+    });    
+        
+    var allArtBox = Backbone.Model.extend({
+        toString: function() { return this.get('title'); }
+    });
+
+    var allTecBox = Backbone.Model.extend({
+        toString: function() { return this.get('title'); }
+    });
+
+    var allSofBox = Backbone.Model.extend({
+        toString: function() { return this.get('title'); }
+    });  
+    
+    var myColArtBox = Backbone.Collection.extend({
+        model: myArtBox,
+        url: '/kandor/empleo/ofertas/senior-matte-painting-artist/?json=get_taxonomy&taxonomy=habilidad-artistica',
+        parse: function (resp) {
+            return _.map( resp.terms, function( item ){
+              return {
+                  id: item.id,
+                  title: item.title
+              };
+            }); 
+        }
+    });
+
+    var myColTecBox = Backbone.Collection.extend({
+        model: myTecBox,
+        url: '/kandor/empleo/ofertas/senior-matte-painting-artist/?json=get_taxonomy&taxonomy=habilidad-tecnica',
+        parse: function (resp) {
+            return _.map( resp.terms, function( item ){
+              return {
+                  id: item.id,
+                  title: item.title
+              };
+            }); 
+        }
+    });
+
+    var myColSofBox = Backbone.Collection.extend({
+        model: mySofBox,
+        url: '/kandor/empleo/ofertas/senior-matte-painting-artist/?json=get_taxonomy&taxonomy=habilidad-software',
+        parse: function (resp) {
+            return _.map( resp.terms, function( item ){
+              return {
+                  id: item.id,
+                  title: item.title
+              };
+            }); 
+        }
+    });
+    
+    var allColArtBox = Backbone.Collection.extend({
+        model: allArtBox,
+        url: '/kandor/empleo//?json=get_taxonomies_index&taxonomy=habilidad-artistica',
+        parse: function (resp) {
+            return _.map( resp.terms, function( item ){
+              return {
+                  id: item.id,
+                  title: item.title
+              };
+            }); 
+        }
+    });
+
+    var allColTecBox = Backbone.Collection.extend({
+        model: allTecBox,
+        url: '/kandor/empleo/?json=get_taxonomies_index&taxonomy=habilidad-tecnica',
+        parse: function (resp) {
+            return _.map( resp.terms, function( item ){
+              return {
+                  id: item.id,
+                  title: item.title
+              };
+            }); 
+        }
+    });
+
+    var allColSofBox = Backbone.Collection.extend({
+        model: allSofBox,
+        url: '/kandor/empleo/?json=get_taxonomies_index&taxonomy=habilidad-software',
+        parse: function (resp) {
+            return _.map( resp.terms, function( item ){
+              return {
+                  id: item.id,
+                  title: item.title
+              };
+            }); 
+        }
+    });
+    
+    var myArtBoxes = new myColArtBox(); 
+    myArtBoxes.fetch();
+    var myTecBoxes = new myColTecBox(); 
+    myTecBoxes.fetch();
+    var mySofBoxes = new myColSofBox(); 
+    mySofBoxes.fetch();
+    
+    var allArtBoxes = new allColArtBox(); 
+    allArtBoxes.fetch();
+    var allTecBoxes = new allColTecBox(); 
+    allTecBoxes.fetch();
+    var allSofBoxes = new allColSofBox(); 
+    allSofBoxes.fetch();
+    
+    function validateEmail(str) {
+        var regex = new RegExp("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+        
+        return regex.test(str) ? null : 'Invalid email';
+    }
+    
+    var Form = Backbone.Model.extend({
+        schema: {
+            nombre:                 {},
+            apellidos:              {},
+            email:                  { type: 'Text', dataType: 'email', validators: ['required', validateEmail] },
+            telefono:               { type: 'Text', dataType: 'tel', validators: ['required'] },
+            nacionalidad:           { type: 'Select', options: ['Española', 'Extranjera'] },
+            link1:                  { type: 'Text', title: 'Enlace a Reel', dataType: 'url' },
+            link2:                  { type: 'Text', title: 'Enlace a Web/Blog', dataType: 'url' },
+            comments:               { type: 'TextArea', title: 'Comentarios', dataType: 'url' },
+            skills1:                { type: 'Checkboxes', title:' ', options: myArtBoxes },
+            skills2:                { type: 'Checkboxes', title:' ', options: myTecBoxes },
+            skills3:                { type: 'Checkboxes', title:' ', options: mySofBoxes },
+            skills4:                { type: 'Checkboxes', title:' ', options: allArtBoxes },
+            skills5:                { type: 'Checkboxes', title:' ', options: allTecBoxes },
+            skills6:                { type: 'Checkboxes', title:' ', options: allSofBoxes },
+            skills7:                { type: 'List' },
+        },
+    });
+    
+    var List = new Form ();
+
+    var Step1View = Backbone.View.extend({
+        id: 'step1',
+
+        events: {
+            'click #next': 'next',
+            'click #hide': 'less',
+            'click #more': 'more'
+        },
+
+        initialize: function (){
+            _.bindAll(this, 'render', 'next', 'more', 'less');
+            console.log(List.attributes);
+            this.render();
+        },
+
+        render: function (){
+            var form1 = new Backbone.Form({
+                model: List,
+                fields: ['skills1']
+            }).render();
+            var form2 = new Backbone.Form({
+                model: List,
+                fields: ['skills2']
+            }).render();
+            var form3 = new Backbone.Form({
+                model: List,
+                fields: ['skills3']
+            }).render();
+            var form4 = new Backbone.Form({
+                model: List,
+                fields: ['skills4']
+            }).render();
+            var form5 = new Backbone.Form({
+                model: List,
+                fields: ['skills5']
+            }).render();
+            var form6 = new Backbone.Form({
+                model: List,
+                fields: ['skills6']
+            }).render();
+            $(this.el).append("<p id='text29'>Las habilidades de los puestos solicitados son las siguientee:");
+            $(this.el).append(form1.el);
+			$(this.el).append(form2.el);
+            $(this.el).append(form3.el);
+			
+            $(this.el).append("<a id='more'></a><div id='show' style='display: none'>"+form4.$el.html()+"<ul>"+form5.$el.html()+"<ul>"+form6.$el.html()+"</div>");
+            $(this.el).append("<br /><a id='next' class='nice radius blue button'>PASO 2</a>");
+            return this;
+        },
+
+		more: function (){
+			$('#more').text('Ocultar').attr('id', 'hide');
+			$('#show').show();
+		},
+		
+		less: function (){
+			$('#hide').text('¿Quieres añadir alguna habilidad más?').attr('id', 'more');
+			$('#show').hide();
+		},
+		
+        next: function (){
+            $('#step1').hide();
+            $('#step2').show();
+            $('#modal-blanket').css({
+                height: $(document).height(), // Span the full document height...
+                width: "100%", // ...and full width
+            });
+        }
+
+    });
+            
+    var List2 = new Form();
+
+    var Step2View = Backbone.View.extend({
+        id: 'step2',
+
+        events: {
+            'click #next2': 'next2',
+        },
+
+        initialize: function (){
+            _.bindAll(this, 'render', 'next2');
+            this.render();
+        },
+        
+        render: function () {
+
+            var form = new Backbone.Form({
+                model: List2,
+                fields: ['nombre', 'apellidos', 'email', 'nacionalidad', 'link1', 'link2']
+            }).render();
+            $(this.el).append("<p id='text36'> Por favor rellena el siguiente formulario para que podamos contactar contigo:<br/>");
+            $(this.el).append(form.el);
+            $(this.el).append("<input id='fileupload' type='file' name='files[]' multiple>");
+            
+            $('#fileupload').fileupload({
+                dataType: 'json',
+                maxNumberOfFiles: '1',
+                forceIframeTransport: true,
+                url: 'server/php/',
+                acceptFileTypes: /\.(pdf|doc|docx)$/i,
+                maxFileSize: '5000',
+                send: function (e, data) {
+                    if (data.files.length > 1) {
+                        return false;
+                    }
+                    if (data.total > 50000) {
+                        return false;
+                    }
+                    alert('File Upload has been canceled');
+                },
+                add: function (e, data) {
+                    var jqXHR = data.submit()
+                        .error(function (jqXHR, textStatus, errorThrown){
+                            if (errorThrown === 'abort') {
+                                alert('File Upload has been aborted');
+                                jqXHR.abort();
+                            }
+                        });
+                },
+                done: function (e, data){
+                }
+            });
+            $(this.el).append("<a id='next2' class='nice radius blue button'>PASO 3</a>");
+            return this;
+        },
+
+        next2: function (){
+            $('#step2').hide();
+            $('#step3').show();
+            $('#modal-blanket').css({
+                top: $(document).scrollTop(),
+                height: $(document).height(), // Span the full document height...
+                width: "100%", // ...and full width
+                
+            });
+        }
+    });
+
+    
+    var List3 = new Form();
+
+    var Step3View = Backbone.View.extend( {
+        id: 'step3',
+        
+        events: {
+            'click #end': 'end',
+        },
+
+        initialize: function (){
+            _.bindAll(this, 'render', 'end');
+            this.render();
+        },
+        
+        render: function () {
+            var form = new Backbone.Form({
+                model: List3,
+                fields: ['comments']
+            }).render();
+            $(this.el).append("<p id='text31'><strong>¡Muchas gracias por tu interés en formar parte del Equipo KANDOR Graphics!</strong>");
+            $(this.el).append("<p id='text32'> Ya tenemos toda la información necesaria para incluirte en nuestro proceso de selección. Será valorada por el Director de Departamento y estaremos encantados de contactar contigo, en caso de que seas preseleccionado. <br/>");
+            $(this.el).append("<p id='text33'> Si quieres añadir alguna cosa más, como una <strong>carta de presentación, recomendaciones o referencias...</strong> introdúcelo a continuación o ve directamente al botón de finalizar.");
+            $(this.el).append(form.el);
+            $(this.el).append("<a id='end' class='nice radius blue button'>FINALIZAR</a>");
+            return this;
+        },
+
+        end: function (){
+            $('#step3').hide();
+        }
+        
+    });
+
+    ModalFormView1 = ModalView.extend({
+        
+        initialize: function(){
+            this.list1View = new Step1View();
+            this.list1View.parentView = this;
+            $(this.el).append(this.list1View.el);
+            this.list2View = new Step2View();
+            this.list2View.parentView = this;
+            $(this.el).append(this.list2View.el);
+            this.list3View = new Step3View();
+            this.list3View.parentView = this;
+            $(this.el).append(this.list3View.el);
+        }
+    });
+
+})(jQuery);
--- kandor-empleo/loop-index.php
+++ kandor-empleo/loop-index.php
@@ -1,10 +1,12 @@
-    <script type="text/javascript">
-$(document).ready(function () {
+<script type="text/javascript">
+	$(document).ready(function () {
 		/* to top */
 		$().UItoTop({ easingType: 'easeOutQuart' });
 	});
 </script>
 
+	<script src="<?php bloginfo('template_url'); ?>/js/form2.js" type="text/javascript"></script>
+	<script src="<?php bloginfo('template_url'); ?>/js/form1.js" type="text/javascript"></script>
 
 <div class="nine columns">
 		<h1><?php _e("<!--:en-->Press<!--:--><!--:es-->Empleo");?></h1>
@@ -33,17 +35,97 @@ $(document).ready(function () {
 <div class="row">		
 	<div class="nine columns cuarenta-top">
 		<dl class="nice contained tabs">
-			<dd><a href="#nice1" class="active-empleo">perfil art&iacute;stico y t&eacute;cnico</a></dd>
+			<dd><a href="" class="active-empleo">perfil art&iacute;stico y t&eacute;cnico</a></dd>
 			<dd><a href="#nice2">otros perfiles </a></dd>
 		</dl>
 
 	<ul class="nice tabs-content contained">
-		<li class="active-empleo" id="nice1Tab"> <!-- perfil artistico tecnico -->
-			<?php $count_posts = wp_count_posts( 'ofertas' )->publish;?>
+		<li class="active-empleo" id="perfin-artistico-tecnico"> <!-- perfil artistico tecnico -->
+			<?php $count_posts = wp_count_posts( 'ofertas' )->publish;?> <!-- /numero de ofertar publicadas/ -->
+			
 			<div class="row" id="mostrar">
-				<p><strong>Mostrando un total de <?php echo $count_posts;?> ofertas laborales de perfil art&iacute;stico y t&eacute;cnico</strong></br>
-				Puedes filtrar los resultados por habilidades requeridas</p>
+				<div class="demo-1">
+				<p><strong>Mostrando un total de <?php echo $count_posts;?> ofertas laborales de perfil art&iacute;stico y t&eacute;cnico.</strong></br>
+				</p>
+				<span class="flecha">Puedes filtrar los resultados por habilidades requeridas <img src="<?php bloginfo('template_url');?>/images/empleo-flecha-filtro.png"></span>
+
+				
+				<div id="filtro">
+				<div class="habilidad">
+					<p>Habilidades art&iacute;sticas</p>
+					<hr class="negro">
+				<?php 
+					$terms = get_terms("habilidad-artistica");
+					$count = count($terms);
+						if ( $count > 0 ){
+			   			echo "<ul>";
+			    		foreach ( $terms as $term ) {
+				?>
+					<?php echo "<div class=items>"; ?>
+					<input type="checkbox" value="" id="">
+ 					<?php echo $term->name;?> 
+ 					<?php echo "</div>"; ?>
+                <?php } echo "</ul>";
+				}?>
+				</div>
+				
+				<div class="habilidad">
+					<p>Habilidades t&eacute;cnicas</p>
+					<hr class="negro">
+				<?php 
+					$terms = get_terms("habilidad-tecnica");
+					$count = count($terms);
+						if ( $count > 0 ){
+			   			echo "<ul>";
+			    		foreach ( $terms as $term ) {
+				?>
+
+				<?php echo "<div class=items>"; ?>
+				<input type="checkbox" value="" id="">
+ 				<?php echo '<a href="'.get_term_link($term->slug, 'habilidad-tecnica').'">'.$term->name.'</a>'?> 
+ 				<?php echo "</div>"; ?>
+                <?php		
+                		
+                		}
+			   			echo "</ul>";
+						}
+				?>
+				</div>
+
+				<div class="habilidad">
+					<p>Manejo de Software</p>
+					<hr class="negro">
+				<?php 
+					$terms = get_terms("habilidad-software");
+					$count = count($terms);
+						if ( $count > 0 ){
+			   			echo "<ul>";
+			    		foreach ( $terms as $term ) {
+				?>
+
+				<?php echo "<div class=items>"; ?>
+				<input type="checkbox" value="" id="">
+ 				<?php echo $term->name;?> 
+ 				<?php echo "</div>"; ?>
+                <?php		
+                		
+                		}
+			   			echo "</ul>";
+						}
+				?>
+				</div>
+				
+				<div class="row">
+					<div class="two columns float-right" id="filtrar">
+						<a class="nice radius blue button full-width" href="#" id="">filtrar</a>
+					</div>
+				</div>
+				
+				</div>
 			</div>
+			
+			</div> <!-- demo-1 -->
+			
   			<?php query_posts( array( 'post_type' => 'ofertas', 'perfiles' => 'artistico-y-tecnico' ) );?>
   			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			<!-- Begin the first article -->
@@ -57,12 +139,10 @@ $(document).ready(function () {
 			$taxo_text = "";  
 			$tipos_list = get_the_term_list( $post->ID, 'departamento', '', ' / ', '' );
 			?>
-			
-
 			<article>
 					<div class="demo-2">
-						<span class="oferta"><?php the_title();?> <span class="departamento"><?php echo $tipos_list; ?></span>
-							<input class="float-right" type="checkbox" value="<?php echo $slug;?>" id="<?php the_id();?>">
+						<span class="oferta"><?php the_title();?> <span class="departamento"><?php echo strip_tags($tipos_list);?></span>
+							<input class="float-right" type="checkbox" value="" id="">
 							<hr>
 						</span>
 						<div class="prueba">
@@ -82,25 +162,27 @@ $(document).ready(function () {
 	    	emm_paginate();
 			} ?>	        	
 			<!-- End Pagination -->
-		</li>
+			</li>
   
   		<li id="nice2Tab"><!-- otros perfiles -->
   			
-  			No hay ofertas
+  		<p>Lo sentimos, pero no tenemos procesos de selecci&oacute;n abiertos para Otros Perfiles.</p>
+		<p>Si est&aacute;s interesado en puestos <strong>Administrativos, Comerciales, Marketing, RRHH, Producci&oacute;n, Finanzas, IT, </strong>etc... puedes inscribirte en nuestra <a href="#" id="windowModal3">Bolsa de Empleo</a> y en cuanto surja una oferta que se ajuste a tu perfil te incluiremos en el proceso de selecci&oacute;n.</p>
   			
 		</li>
 	</ul>
 	<div class="row cuarenta-top">
-	<div class="four columns">
-		<p><strong>Selecciona las ofertas de la lista y</strong></p>
+	<div class="five columns">
+		<p class="selecciona"><strong>Selecciona las ofertas de la lista y</strong></p>
 	</div>
 	<div class="four columns" id="volver">
-		<a class="nice radius blue button float-right full-width" href="#" id="windowModalForm1">solicita estos puestos</a>
+		<a class="nice radius blue button float-right full-width" href="#" id="windowModal1">solicita estos puestos</a>
 	</div>
 	</div>
 	<div class="row cuarenta-top">
 <p><strong>&iquest;No has encontrado ninguna oferta que se ajuste a tu perfil?</strong></p>
-<p>No te preocupes en KANDOR Graphics estamos continuamente buscando nuevos talentos para nuestros proyectos. Inscr&iacute;bete en nuestra <a href="#" id="windowModalForm2">Bolsa de Empleo</a> y en cuanto surja una oferta que se ajuste a tu perfil te incluiremos en el proceso de selecci&oacute;n.</p>
+<p>No te preocupes en KANDOR Graphics estamos continuamente buscando nuevos talentos para nuestros proyectos. Inscr&iacute;bete en nuestra
+<a href="#" id="windowModal2">Bolsa de Empleo</a> y en cuanto surja una oferta que se ajuste a tu perfil te incluiremos en el proceso de selecci&oacute;n.</p>
 
 <p>Si deseas estar informado acerca de nuestras ofertas de empleo suscr&iacute;bete a esta lista y estar&aacute;s siempre al tanto de nuestros procesos de selecci&oacute;n.</p>
 </div>
@@ -127,29 +209,70 @@ $(".demo-2").collapse({show: function(){
 	    }
 	});
 </script>
+
+<script type="text/javascript">
+$(".demo-1").collapse({show: function(){
+	        this.animate({
+	            opacity: 'toggle',
+	            height: 'toggle'
+	        }, 300);
+	    },
+	    hide : function() {
+			this.animate({
+	            opacity: 'toggle', 
+	            height: 'toggle'
+	        }, 300);
+	    }
+	});
+</script>
+
 <script type="text/javascript">
 $(document).ready(
     function(){
-    $('#windowModalForm1').click(function(event){
-        event.preventDefault();
-        event.stopPropagation();
+    $('#windowModal1').click(function(event) {
 
-        var view = new ModalViewForm1();
+		$('html, body').animate({ scrollTop: 0 }, 'slow');
+		
+		event.preventDefault();
+        event.stopPropagation();
+		
+		var view = new ModalFormView1();
         view.render().showModal({
             y: 20,
             localWidth: 780
         });
-    });
+
+    });     
 });
-</script>
-<script type="text/javascript">
+
 $(document).ready(
     function(){
-    $('#windowModalForm2').click(function(event){
-        event.preventDefault();
+    $('#windowModal2').click(function(event) {
+    
+		$('html, body').animate({ scrollTop: 0 }, 'slow');		
+		
+		event.preventDefault();
         event.stopPropagation();
+		
+		var view = new ModalFormView2();
+        view.render().showModal({
+            y: 20,
+            localWidth: 780
+        });
 
-        var view = new ModalViewForm2();
+    });     
+});
+
+$(document).ready(
+    function(){
+    $('#windowModal3').click(function(event) {
+    
+		$('html, body').animate({ scrollTop: 0 }, 'slow');		
+		
+		event.preventDefault();
+        event.stopPropagation();
+		
+		var view = new ModalFormView2();
         view.render().showModal({
             y: 20,
             localWidth: 780
@@ -158,5 +281,3 @@ $(document).ready(
     });     
 });
 </script>
-
-
